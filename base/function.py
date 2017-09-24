361dd98f611c3f8805ec160de39277e9a153e18d
cloudtian <624714349@qq.com>
2017-09-24 19:04:12 +0800

    learn python:function params and recursion

--- base/function.py
+++ base/function.py
@@ -111,14 +111,50 @@ person('Michael', 30)
 person('Bob', 35, city='Beijing')
 person('Adam', 45, city='Beijing', job='Engineer')
 
+#命名关键字参数
+def person(name, age, **kw):
+	if 'city' in kw:
+		#有city参数
+		pass
+	if 'job' in kw:
+		#有job参数
+		pass
+	print('name:', name, 'age:', age, 'other:', kw)
 
+person('jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
 
+#如果要限制关键字参数的名字，就可以用命名关键字参数，例如只接受city和job作为关键字参数
+def person(name,age,*,city,job):
+	print(name,age,city,job)
+#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+person('Jack',24,city='Beijing',job='Engineer')
 
+#参数组合：必选参数，默认参数，可变参数，命名关键字参数，关键字参数
+#*args是可变参数，args接收的是一个tuple
+#**kw是关键字参数，kw接收的是一个dict
+#可变参数既可以直接传入：func(1,2,3)，又可以先组装list或tuple,再通过*args传入：func(*(1,2,3))
+#关键字参数既可以直接传入：func(a=1,b=2),又可以先组装dict,再通过**kw传入：func(**{'a'=1,'b'=2})
 
+#递归函数
+def fact(n):
+	if n == 1:
+		return 1
+	return n * fact(n-1)
+#使用递归函数需要注意防止栈溢出。在计算机中，函数调用是通过栈（stack）这种数据结构实现的，
+#每当进入一个函数调用，栈就会加一层栈帧，每当函数返回，栈就会减一层栈帧。
+#由于栈的大小不是无限的，所以，递归调用的次数过多，会导致栈溢出。
 
+#解决递归调用栈溢出的方法是通过尾递归优化，事实上尾递归和循环的效果是一样的，所以，把循环看成是一种特殊的尾递归函数也是可以的。
+#尾递归是指，在函数返回的时候，调用自身本身，并且，return语句不能包含表达式。
+#这样，编译器或者解释器就可以把尾递归做优化，使递归本身无论调用多少次，都只占用一个栈帧，不会出现栈溢出的情况。
 
-
-
-
-
+def fact(n):
+	return fact_iter(n,1)
+def fact_iter(num, product):
+	if num == 1:
+		return product
+	return fact_iter(num-1,num*product)
+#尾递归调用时，如果做了优化，栈不会增长，因此，无论多少次调用也不会导致栈溢出。
+#遗憾的是，大多数编程语言没有针对尾递归做优化，Python解释器也没有做优化，
+#所以，即使把上面的fact(n)函数改成尾递归方式，也会导致栈溢出。
 

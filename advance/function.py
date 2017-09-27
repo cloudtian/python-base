04c51c0c74ef0b4dde10887300f1fcc4f13e63fc
cloudtian <cloud_tian@foxmail.com>
2017-09-27 17:57:43 +0800

    learn python:function map/reduce,filter,sorted

--- advance/function.py
+++ advance/function.py
@@ -38,7 +38,121 @@ print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 
 #对一个序列求和
 from functools import reduce
-def add(x, y):
+
+def add2(x, y):
     return x + y
-print('reduce：' ,reduce(add, [1, 3, 5, 7, 9])) #求和有内建函数sum()
+print('reduce：', reduce(add2, [1, 3, 5, 7, 9])) #求和有内建函数sum()
 print('sum:', sum([x for x in range(1, 10, 2)]))
+#把序列[1,3,5,7,9]变成整数13579
+def fn(x, y):
+    return x * 10 + y
+print(reduce(fn, [1, 3, 5, 7, 9]))
+
+def char2num(s):
+    return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9': 9}[s]
+print(reduce(fn, map(char2num, '13579')))
+
+#整理成一个str2int的函数：
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9': 9}[s]
+    return reduce(fn, map(char2num, s))
+
+#还可以用lambda函数进一步简化
+def str2intLambda(s):
+    return reduce(lambda x, y: x * 10, map(char2num, s))
+
+#练习：利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字
+def normalize(name):
+    return name[0].upper() + name[1:].lower()
+L1 = ['adam', 'LISA', 'barT']
+print('before normalize:', L1)
+L2 = list(map(normalize, L1))
+print('after normalize:', L2)
+
+#练习，请编写一个prod()函数，可以接受一个list并利用reduce()求积
+def prod(L):
+    def ji(x, y):
+        return x * y
+    return reduce(ji, L)
+print('3 * 5 * 7 * 9 = ', prod([3, 5, 7, 9]))
+
+#练习：利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456
+def str2float(s):
+    def char2num(s):
+        return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9': 9}[s]
+    def fn(x, y):
+        return x * 10 + y
+    floatLength = len(s.split('.')[1])
+    intMod = reduce(fn, map(char2num, s.split('.')[0]))
+    floatMod = reduce(fn, map(char2num, s.split('.')[1]))
+    return intMod + floatMod * (0.1**floatLength)
+print('str2float(\'123.456\') = ', str2float('123.456'))
+
+#filter 用于过滤序列。filter()把传入的函数一次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃
+
+#把一个序列中的空字符串删掉
+def not_empty(s):
+    return s and s.strip()
+print(list(filter(not_empty, ['A', '', 'B', None, 'C', ' '])))
+#filter()函数返回的是一个Iterator,也就是一个惰性序列，所以要强迫filter()完成计算结果，需要用list()函数获取所有结果并返回list
+
+#用filter求素数
+#生成器，无限序列
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+#筛选函数
+def _not_divisible(n):
+    return lambda x: x % n > 0
+#生成器，不断返回下一个素数
+def primes():
+    yield 2
+    it = _odd_iter() #初始化序列
+    while True:
+        n = next(it) #返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) #构造新序列
+
+#打印10以内的素数
+for n in primes():
+    if n < 10:
+        print(n)
+    else:
+        break
+
+#练习:回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数
+def is_palindrome(n):
+    strNum = str(n)
+    result = True
+    if len(strNum) % 2 == 0:
+        result = False
+    else:
+        n = 0
+        while n < (len(strNum) - 1) / 2:
+            if strNum[n] != strNum[-n - 1]:
+                result = False
+                break
+            n = n + 1
+    return result 
+def is_palindrome2(n):
+    return str(n) == str(n)[::-1]
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
+output = filter(is_palindrome2, range(1, 1000))
+print(list(output))
+
+#sorted() 排序函数
+print(sorted([36, 5, -12, 9, -21]))
+#sorted()函数也是一个高阶函数，可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序
+print(sorted([36, 5, -12, 9, -21], key=abs))
+
+#默认情况下，对字符串排序，是按照ASCII的大小比较的
+
+#忽略大小写的排序
+print(sorted(['bob', 'ablout', 'Zoo', 'Credit'], key=str.lower))
+

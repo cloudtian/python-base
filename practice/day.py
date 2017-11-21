1658822d45dcdebece6f7c3962a4e1c82385b310
cloudtian <cloud_tian@foxmail.com>
2017-11-21 22:31:10 +0800

    practice -- 编写完成web app的步骤，添加macro pagination

--- practice/day.py
+++ practice/day.py
@@ -99,3 +99,28 @@
 #利用Python自带的subprocess实现进程的启动和终止，并把输入输出重定向到当前进程的输入输出中
 #python pymonitor.py wsgiapp.py
 #./pymonitor.py app.py
+
+#Day 14 - 完成Web App
+#在Debug开发模式下完成后端所有API，前端所有页面
+#后端API包括：
+#获取日志：GET /api/blogs
+#创建日志：POST /api/blogs
+#修改日志：POST /api/blogs/:blog_id
+#删除日志：POST /api/blogs/:blog_id/delete
+#获取评论：GET /api/comments
+#创建评论：POST /api/blogs/:blog_id/comments
+#删除评论：POST /api/comments/:comment_id/delete
+#创建新用户：POST /api/users
+#获取用户： GET /api/users
+#管理页面包括：
+#评论列表页: GET /manage/comments
+#日志列表页：GET /manage/blogs
+#创建日志页：GET /manage/blogs/create
+#修改日志页：GET /manage/blogs/
+#用户列表页：GET /manage/users
+#用户浏览页面包括：
+#注册页：GET /register
+#登录页：GET /sigin
+#注销页：GET /signout
+#首页：GET /
+#日志详情页：GET /blog/:blog_id
